10e256b78e07d4f0130db438a4d24d90ea5f8dba
Uriel <uventura@simetrical.com.mx>
2019-07-05 17:58:15 -0500

    Added navigations

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Bookmarks from '@material-ui/icons/Bookmarks';
 import MyTheme from './theme';
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import Container from "./containers";
 
 function App() {
   return (
     <MuiThemeProvider theme={MyTheme}>
-      {console.log(MyTheme)}
-      <Button variant="contained" color='secondary'>
-      <Bookmarks></Bookmarks>
-          Chaper 2
-      </Button>
+      <Container />
     </MuiThemeProvider>
   );
 }
---
+++ src/containers/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BottomNavigation from "../components/bottomNavigation";
+import TopNavigation from "../components/topNavigation";
+
+const Container = () => {
+  return (
+   <div>
+       <TopNavigation />
+       <BottomNavigation />
+   </div>   
+  );
+};
+
+export default Container;

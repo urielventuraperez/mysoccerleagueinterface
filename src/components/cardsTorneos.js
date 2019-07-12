0752e8ae51086f614406e2a7b8921acc2a100d16
Uriel <uventura@simetrical.com.mx>
2019-07-12 16:58:23 -0500

    Add teams init

--- src/components/cardsTorneos.js
+++ src/components/cardsTorneos.js
@@ -1,21 +1,44 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 
 const CardTorneos = props => {
+  
+  const handleClick = event => {
+    console.log(event.currentTarget);
+  };
+
+
   return (
     <Grid container spacing={3}>
       {props.loop.map(l => {
         return (
           <Grid key={l.id} item xs={12} md={12}>
             <Card className={props.card}>
+              <CardHeader
+                action={
+                  <IconButton
+                  component={Link}
+                  to={{ pathname: `/torneo/${l.id}/agregarEquipo` }}
+                    onClick={event => handleClick(event)}
+                    aria-label="Settings"
+                  >
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                title={l.nombre}
+                subheader={l.categoria}
+              />
               <CardActionArea>
                 <CardMedia
                   className={props.media}
@@ -23,22 +46,25 @@ const CardTorneos = props => {
                   title="Contemplative Reptile"
                 />
                 <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {l.nombre}
-                  </Typography>
+                  {" "}
                   <Typography
                     variant="body2"
                     color="textSecondary"
                     component="p"
-                  />
+                  >
+                    {l.responsable_nombre} {l.responsable_apellido}
+                  </Typography>
                 </CardContent>
               </CardActionArea>
               <CardActions>
                 <Button
                   component={Link}
-                  to={{pathname: `/torneo/${l.id}/equipos`,   state: { 
-                    cardNombre: l.nombre
-                  }}}
+                  to={{
+                    pathname: `/torneo/${l.id}/equipos`,
+                    state: {
+                      cardNombre: l.nombre
+                    }
+                  }}
                   size="small"
                   color="primary"
                 >
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import Home from '../pages/home'
 import EquiposTorneo from '../pages/equiposTorneo'
 import EstadisticasEquipo from '../pages/estadisticasEquipo'
 import Administracion from '../pages/administracion'
+import AgregarEquipo from '../pages/agregarEquipo'
 
 const routes = (
   <div>
@@ -16,6 +17,7 @@ const routes = (
       <Route path="/torneo/:torneoId/equipos" component={EquiposTorneo} />
       <Route path="/estadisticas/equipo/:equipoId" component={EstadisticasEquipo} />
       <Route path="/administracion" component={Administracion} />
+      <Route path='/torneo/:torneoId/agregarEquipo' component={AgregarEquipo} />
     </Switch>
   </div>
 )
